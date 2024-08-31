426d56b68777e30018cdc33042fa36a06a2fc7c4
Alexey261 <dlyx@yandex.ru>
2024-08-31 21:26:38 +0300

    Add files via upload

--- module_8_1.py
+++ module_8_1.py
@@ -4,13 +4,14 @@ def add_everything_up(a,b):
     except TypeError:
         result = str(a)+str(b)
     else:
-        result = round(a + b, 3)
+        result = a + b if type(a + b) is str else round(a + b, 3)
 
     return result
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
+print(add_everything_up('123.456', '7'))
 
 
 
